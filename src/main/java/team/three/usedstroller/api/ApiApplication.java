b02cd56932668b7bba0f69c9ac2d461b6cb40d51
hoonyhoney <hoonyhoeny@gmail.com>
2024-01-17 08:24:41 +0900

    fix : Cors관련오류 설정 - localhost:3000추가 , allowCredential 값 true

--- src/main/java/team/three/usedstroller/api/ApiApplication.java
+++ src/main/java/team/three/usedstroller/api/ApiApplication.java
@@ -3,7 +3,10 @@ package team.three.usedstroller.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -11,6 +14,21 @@ public class ApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ApiApplication.class, args);
+  }
+
+
+  /**
+   CORS오류 관련설정
+   */
+  @Bean
+  public WebMvcConfigurer corsConfigurer(){
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+                .allowCredentials(true);
+      }
+    };
   }
 
 }
