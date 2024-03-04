31fd438b79d242aa247ac7d4b36d6e7cac18944c
hoonyhoney <hoonyhoeny@gmail.com>
2024-03-05 07:57:54 +0900

    Fix: redis설정 주석처리, application-prod.yml 추가

--- src/main/java/team/three/usedstroller/api/config/RedisConfig.java
+++ src/main/java/team/three/usedstroller/api/config/RedisConfig.java
@@ -15,7 +15,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
+//@Configuration
 @EnableCaching
 public class RedisConfig {
 
