9af4a6d079558d1ebf934bd37cf0cda892c6734d
clowncdi <clowncdi85@gmail.com>
2024-09-08 10:35:31 +0900

    fix: 공통 path 설정 제거

--- src/main/java/team/three/usedstroller/api/users/controller/UsersController.java
+++ src/main/java/team/three/usedstroller/api/users/controller/UsersController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team.three.usedstroller.api.users.dto.AccountDto;
@@ -16,6 +17,7 @@ import team.three.usedstroller.api.users.service.AccountService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UsersController {
 
   private final AccountService accountService;
