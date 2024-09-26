d473a060f299da8307d46649fae1932daa2eee35
hoonyhoney <hoonyhoeny@gmail.com>
2024-09-27 06:33:36 +0900

    minor

--- src/test/java/team/three/usedstroller/api/QueryDslTest.java
+++ src/test/java/team/three/usedstroller/api/QueryDslTest.java
@@ -59,12 +59,12 @@ class QueryDslTest {
         .fetch();
   }
 
-  @Test
-  void cotainTest(){
-    FilterReq filter = new FilterReq("",null,null,null,"", "", "전국",null,null,null);
-    Pageable pageable = PageRequest.of(1,1);
-    Page<ProductRes> products = productRepository.getProducts(filter, pageable);
-  }
+//  @Test
+//  void cotainTest(){
+//    FilterReq filter = new FilterReq("",null,null,null,"", "", "전국",null,null,null);
+//    Pageable pageable = PageRequest.of(1,1);
+//    Page<ProductRes> products = productRepository.getProducts(filter, pageable);
+//  }
 
   @Test
   public void subQuery(){
@@ -86,17 +86,6 @@ class QueryDslTest {
 
   @Test
   void recommendPrice(){
-//    List<Product> fetch = query.selectFrom(product)
-//        .leftJoin(model).on(product.model.id.eq(model.id))
-//        .where(
-//                 product.model.id.isNotNull()
-//                .and(model.recommendPrice.isNotNull())
-//                .and(Expressions.numberTemplate(Double.class, "CAST({0} AS DOUBLE)", product.price)
-//                    .gt(model.recommendPrice.multiply(0.9)))
-//                .and(Expressions.numberTemplate(Double.class, "CAST({0} AS DOUBLE)", product.price)
-//                    .lt(model.recommendPrice.multiply(1.1)))
-//        ).fetch();
-
     List<Product> result = query.selectFrom(product)
         .leftJoin(product.model, model)
         .where(
@@ -104,18 +93,19 @@ class QueryDslTest {
             model.recommendPrice.isNotNull(),
             // recommendPrice * 0.9를 BigDecimal로 처리한 후 Long으로 변환
             product.price.gt(
-                Expressions.numberTemplate(BigDecimal.class, "({0} * 0.9)", model.recommendPrice).longValue()
+                Expressions.numberTemplate(BigDecimal.class, "({0} * 0.7)", model.recommendPrice).longValue()
             ),
             // recommendPrice * 1.1을 BigDecimal로 처리한 후 Long으로 변환
             product.price.lt(
-                Expressions.numberTemplate(BigDecimal.class, "({0} * 1.1)", model.recommendPrice).longValue()
-            )
+                Expressions.numberTemplate(BigDecimal.class, "({0} * 1.2)", model.recommendPrice).longValue()
+            ),
+            product.title.notLike("%배시넷").and(product.title.notLike("%베시넷%"))
+
         )
         .fetch();
     for (Product fetch1 : result) {
       System.out.println("fetch1 = " + fetch1);
     }
-
   }
 
 
