1c8d4e447a150b7e26bb42e15206f1e2b4cdd15b
hoonyhoney <hoonyhoeny@gmail.com>
2025-04-17 22:29:14 +0900

    cors2

--- src/main/java/team/three/usedstroller/api/security/config/SecurityConfig.java
+++ src/main/java/team/three/usedstroller/api/security/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
 
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
-    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "https://jungmocha.co.kr"));
+    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "https://jungmocha.co.kr","https://front-git-feautre-kakaooneclick-donghuns-projects.vercel.app/"));
     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
     corsConfiguration.setAllowCredentials(true);
